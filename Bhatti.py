0cd499133c2f700ed78eff660c02ba6d7300c4b0
Bhatti <111224095+noorkhan173@users.noreply.github.com>
2022-08-14 15:54:37 +0500

    Create Bhatti.py

---
+++ Bhatti.py
@@ -0,0 +1,32 @@
+import os,time,platform
+print('\n\x1b[1;37m[•] Checking Update...');time.>
+os.system('git pull')
+os.system('xdg-open https://www.facebook.com/grou>
+logo = ("""\033[1;37m    ###    ##    ## #### ## >
+   ## ##   ##   ##   ##  ###   ## ##    ##
+  ##   ##  ##  ##    ##  ####  ## ##
+ ##     ## #####     ##  ## ## ## ##   ####
+ ######### ##  ##    ##  ##  #### ##    ##
+ ##     ## ##   ##   ##  ##   ### ##    ##
+ ##     ## ##    ## #### ##    ##  ######  ❤
+(!)══════════════════════════════════════════
+(!) Author   : IMTIAZ AKING
+(!) Guthub   : AKING110
+(!) Facebook : MR.Ansh.06
+e     : PAID
+(!) Version  : 1.2.2                        \033[1;37m(!)══════════════════════════════>
+
+def Run():
+        bit = platform.architecture()[0]
+        os.system('clear')
+        print(logo)
+        print('[•] Choose Your Country For >        print('[1] Pak Cloning \n[2] BD Clo>
+        Aking = input('[•] Choose : ')
+        if Aking =='1':
+                if bit =='32bit':                                   import AKING32
+                elif bit =='64bit':                                 import AKING
+        elif Aking =='2':
+        if bit =='32bit':
+                        import BD32
+                elif bit =='64bit':                                 import BD
+Run()
